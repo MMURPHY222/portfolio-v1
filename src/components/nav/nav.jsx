83a6a4636b6e7ed2d762b02c75ee6cfe90e3280b
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-09-15 16:57:42 -0400

    styling nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,26 +1,60 @@
 import React from 'react'
 import '../nav/nav.css'
-import { Person, Mail } from '@material-ui/icons'
+import { Mail } from '@material-ui/icons'
 
 export default function Nav() {
     return (
         <div className='nav'>
+        <div className="wrapper">
+            <div className="left">
+                <a href="#intro" className="logo">MMURPHY.</a>
+            </div>
+            <div className="right">
+                <ul>
+                    <li><a href="#"> Home</a></li>
+                    <li><a href="#"> About</a></li>
+                    <li><a href="#"> Work</a></li>
+                    <li><a href="#"> Project</a></li>
+                </ul>
+            </div>
+            <div className="burger">
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+{/* <div className='nav'>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">MMURPHY.</a>
-                    <div className="item">
-                        <Person className="icon"/>
-                        <span>+803 457 4113</span>
-                    </div>
-                    <div className="mail">
-                        <Mail className="icon"/>
-                        <span>meghanmurphy.212@gmail.com</span>
-                    </div>
                 </div>
                 <div className="right">
-
+                    <ul>
+                        <li><a href="#"> Home</a></li>
+                        <li><a href="#"> About</a></li>
+                        <li><a href="#"> Work</a></li>
+                        <li><a href="#"> Project</a></li>
+                    </ul>
                 </div>
             </div>
-        </div>
-    )
-}
+        </div> */}
+
+
+
+    //     <div>
+    //     <nav>
+    //         <div className="logo">
+    //             <h4>MMURPHY.</h4>
+    //         </div>
+    //         <ul>
+    //             <li><a href="#"> Home</a></li>
+    //             <li><a href="#"> About</a></li>
+    //             <li><a href="#"> Work</a></li>
+    //             <li><a href="#"> Project</a></li>
+    //         </ul>
+    //     </nav>
+    // </div>
