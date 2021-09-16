66d0d1fb2e5a897c276f01452ae92f3c4ae7a659
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-09-16 10:40:56 -0400

    updated menu components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Intro from './components/intro/Intro'
 import Portfolio from './components/portfolio/Portfolio'
 import Works from './components/works/Works'
 import Contact from './components/contact/Contact'
+import Menu from './components/menu/Menu'
 import './app.css'
 import './global.css'
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="app">
         <Nav menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <Menu />
         <div className='sections'>
           <Intro />
           <Portfolio />
---
+++ src/components/menu/menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../menu/menu.css'
+
+export default function Menu() {
+    return (
+        <div className="menu">
+            <ul>
+                <li><a href='#intro'>Intro</a></li>
+                <li><a href='#portfolio'>Portfolio</a></li>
+                <li><a href='#works'>Works</a></li>
+                <li><a href='#contact'>Contact</a></li>
+            </ul>
+
+        </div>
+    )
+}
