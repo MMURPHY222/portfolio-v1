aa05f790096b6e96c2c5530e0323cd698d90cf8f
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-09-16 10:52:38 -0400

    menu functionality

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import '../menu/menu.css'
 
-export default function Menu() {
+export default function Menu({ menuOpen, setMenuOpen }) {
     return (
-        <div className="menu">
+        <div className={ 'menu ' + (menuOpen && 'active')}>
             <ul>
-                <li><a href='#intro'>Intro</a></li>
-                <li><a href='#portfolio'>Portfolio</a></li>
-                <li><a href='#works'>Works</a></li>
-                <li><a href='#contact'>Contact</a></li>
+                <li onClick={()=>setMenuOpen(false)}><a href='#intro'>Intro</a></li>
+                <li onClick={()=>setMenuOpen(false)}><a href='#portfolio'>Portfolio</a></li>
+                <li onClick={()=>setMenuOpen(false)}><a href='#works'>Works</a></li>
+                <li onClick={()=>setMenuOpen(false)}><a href='#contact'>Contact</a></li>
             </ul>
 
         </div>
