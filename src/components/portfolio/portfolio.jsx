677b279592b3f8ecd537ec9ce99e1686a366a9df
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-09-16 22:42:18 -0400

    portfolio list

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,18 +1,51 @@
 import React from 'react'
 import '../portfolio/portfolio.css'
-import photo from '../../assets/otc.png'
+import photo from '../../assets/otc2.png'
 
 export default function Portfolio() {
+    const list = [
+        {
+            id: 'featured',
+            title: 'Featured',
+        }, 
+        {
+            id: 'reactApps',
+            title: 'React Apps',
+        },
+        {
+            id: 'personalProjects',
+            title: 'Personal Projects'
+        },
+        {
+            id: 'classWork',
+            title: 'Class Work'
+        }
+    ]
     return (
         <div className='portfolio' id='portfolio'>
             <h1>Portfolio</h1>
-            <ul>
-                <li className="activePort">Featured</li>
-                <li>React Apps</li>
-                <li>Personal Projects</li>
-                <li>Class Work</li>
-            </ul>
+            <ul></ul>
             <div className="portfolioContainer">
+                <div className="portItem">
+                    <img src={photo} alt =''></img>
+                    <h3>Outdoor Trading Co.</h3>
+                </div>
+                <div className="portItem">
+                    <img src={photo} alt =''></img>
+                    <h3>Outdoor Trading Co.</h3>
+                </div>
+                <div className="portItem">
+                    <img src={photo} alt =''></img>
+                    <h3>Outdoor Trading Co.</h3>
+                </div>
+                <div className="portItem">
+                    <img src={photo} alt =''></img>
+                    <h3>Outdoor Trading Co.</h3>
+                </div>
+                <div className="portItem">
+                    <img src={photo} alt =''></img>
+                    <h3>Outdoor Trading Co.</h3>
+                </div>
                 <div className="portItem">
                     <img src={photo} alt =''></img>
                     <h3>Outdoor Trading Co.</h3>
