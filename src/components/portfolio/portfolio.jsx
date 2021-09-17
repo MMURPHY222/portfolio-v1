19a1549471a70bff1ec45adbf506113072d93edd
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-09-16 22:04:09 -0400

    ground work for portfolio page

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import '../portfolio/portfolio.css'
+import photo from '../../assets/otc.png'
 
 export default function Portfolio() {
     return (
         <div className='portfolio' id='portfolio'>
             <h1>Portfolio</h1>
             <ul>
-                <li>Featured</li>
+                <li className="activePort">Featured</li>
                 <li>React Apps</li>
                 <li>Personal Projects</li>
                 <li>Class Work</li>
             </ul>
+            <div className="portfolioContainer">
+                <div className="portItem">
+                    <img src={photo} alt =''></img>
+                    <h3>Outdoor Trading Co.</h3>
+                </div>
+            </div>
         </div>
     )
 }
