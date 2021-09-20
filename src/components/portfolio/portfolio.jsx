e69ad7f0f3b0905214ed372fc886d56aaf0c4bbc
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-09-20 11:04:34 -0400

    portofolio functionality rendering correctly with dummy data

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,9 +3,18 @@ import { useState } from 'react'
 import '../portfolio/portfolio.css'
 import photo from '../../assets/otc2.png'
 import PortfolioList from '../portfolioList/PortfolioList'
+import {
+    featuredPortfolio, 
+    reactAppsPortfolio,
+    personalProjectsPortfolio,
+    classWorkPortfolio
+} from '../../data'
+import { useEffect } from 'react'
 
 export default function Portfolio() {
-    const [selected, setSelected] = useState('featured')
+    const [selected, setSelected] = useState('featured');
+    const [data, setData] = useState([]);
+
     const list = [
         {
             id: 'featured',
@@ -24,6 +33,26 @@ export default function Portfolio() {
             title: 'Class Work'
         }
     ]
+
+    useEffect(() => {
+        switch(selected) {
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+            case "reactApps":
+                setData(reactAppsPortfolio);
+                break;
+            case "personalProjects":
+                setData(personalProjectsPortfolio);
+                break;
+            case "classWork":
+                setData(classWorkPortfolio);
+                break;
+            default:
+                setData(featuredPortfolio);
+        }
+    }, [selected])
+
     return (
         <div className='portfolio' id='portfolio'>
             <h1>Portfolio</h1>
@@ -39,30 +68,12 @@ export default function Portfolio() {
                 ))}
             </ul>
             <div className="portfolioContainer">
+                {data.map((el) => (
                 <div className="portItem">
-                    <img src={photo} alt =''></img>
-                    <h3>Outdoor Trading Co.</h3>
+                    <img src={el.img} alt =''></img>
+                    <h3>{el.title}</h3>
                 </div>
-                <div className="portItem">
-                    <img src={photo} alt =''></img>
-                    <h3>Outdoor Trading Co.</h3>
-                </div>
-                <div className="portItem">
-                    <img src={photo} alt =''></img>
-                    <h3>Outdoor Trading Co.</h3>
-                </div>
-                <div className="portItem">
-                    <img src={photo} alt =''></img>
-                    <h3>Outdoor Trading Co.</h3>
-                </div>
-                <div className="portItem">
-                    <img src={photo} alt =''></img>
-                    <h3>Outdoor Trading Co.</h3>
-                </div>
-                <div className="portItem">
-                    <img src={photo} alt =''></img>
-                    <h3>Outdoor Trading Co.</h3>
-                </div>
+                ))}
             </div>
         </div>
     )
