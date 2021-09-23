e5293ad18a0de11e21256668780e2863af817183
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-09-23 19:52:50 -0400

    added titles to class work data.js array

--- src/data.js
+++ src/data.js
@@ -140,40 +140,46 @@ export const featuredPortfolio = [
   
   export const classWorkPortfolio = [
     {
-      id: 1,
-      title: "Design Social Media App",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+        id: 1,
+        title: "Password Generator",
+        img: solid,
+        gitHub: '',
+        liveLink: ''
     },
     {
-      id: 2,
-      title: "Design Rampa UI Design",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+        id: 2,
+        title: "Fitness Tracker",
+        img: solid,
+        gitHub: '',
+        liveLink: ''
     },
     {
-      id: 3,
-      title: "Design E-commerce Web Design",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+        id: 3,
+        title: "Note Taker",
+        img: solid,
+        gitHub: '',
+        liveLink: ''
     },
     {
-      id: 4,
-      title: "Design Relax Mobile App",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+        id: 4,
+        title: "Montana Quiz",
+        img: solid,
+        gitHub: '',
+        liveLink: ''
     },
     {
-      id: 5,
-      title: "Design Keser Web Design",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+        id: 5,
+        title: "Day Planner",
+        img: solid,
+        gitHub: '',
+        liveLink: ''
     },
     {
-      id: 6,
-      title: "Design Banking App",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+        id: 6,
+        title: "Password Generator",
+        img: solid,
+        gitHub: '',
+        liveLink: ''
     },
   ];
   
