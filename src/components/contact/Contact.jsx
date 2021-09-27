8ce0d3583b1b56b74690fcaf4505a379bc2cfd31
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-09-27 11:58:00 -0400

    completed contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import '../contact/contact.css'
+import bikeMessenger from '../../assets/bikeMess.png'
 
 export default function Contact() {
+
     return (
         <div className='contact' id='contact'>
-
+            <div className="contactLeft">
+                <img src={bikeMessenger} alt="bikePhoto" />
+            </div>
+            <div className="contactRight">
+                <h2>Contact.</h2>
+                <div className="email"> <a href="meghanmurphy.212@gmail.com"> meghanmurphy.212@gmail.com </a> </div>
+                <div className="linkedIn"> <a href="https://www.linkedin.com/in/meghan-murphy-111445179/"> LinkedIn </a> </div>
+                <div className="gitHub"> <a href="https://github.com/MMURPHY222"> GitHub</a> </div>
+            </div>
         </div>
     )
 }
