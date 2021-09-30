2951ccae3ba75ac93873c32a9e2f0ac041db3547
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-09-30 12:42:21 -0400

    added link for pdf resume

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../contact/contact.css'
 import bikeMessenger from '../../assets/bikeMess.png'
+import resume from '../../assets/resume.pdf'
 
 export default function Contact() {
 
@@ -11,9 +12,10 @@ export default function Contact() {
             </div>
             <div className="contactRight">
                 <h2>Contact.</h2>
-                <div className="email"> <a href="meghanmurphy.212@gmail.com"> meghanmurphy.212@gmail.com </a> </div>
-                <div className="linkedIn"> <a href="https://www.linkedin.com/in/meghan-murphy-111445179/"> LinkedIn </a> </div>
-                <div className="gitHub"> <a href="https://github.com/MMURPHY222"> GitHub</a> </div>
+                <div className="email"> <a href="meghanmurphy.212@gmail.com" target="_blank" rel="noreferrer"> meghanmurphy.212@gmail.com </a> </div>
+                <div className="linkedIn"> <a href="https://www.linkedin.com/in/meghan-murphy-111445179/" target="_blank" rel="noreferrer"> LinkedIn </a> </div>
+                <div className="gitHub"> <a href="https://github.com/MMURPHY222" target="_blank" rel="noreferrer"> GitHub</a> </div>
+                <div className="resume"> <a href={resume} target="_blank" rel="noreferrer"> Resume </a> </div>
             </div>
         </div>
     )
